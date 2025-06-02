9989db5af25a9cfd8e995bb304d658a4b6761586
zk18l <zaidalpero63@gmail.com>
2025-06-02 21:07:39 +0300

    Complete the 5 exercises

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,18 +8,27 @@
 */
 
 // Add your code right below, good luck!
-
-
-
-
-
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+const greeting =
+  "Hello! My name is " +
+  firstName +
+  " " +
+  lastName +
+  " and I am " +
+  (thisYear - birthYear) +
+  " years old.";
+const fullName = firstName +" "+ lastName;
+const age = thisYear - birthYear;
 // Do not change this
 module.exports = {
-  firstName: typeof firstName === 'undefined' ? undefined : firstName,
-  lastName: typeof lastName === 'undefined' ? undefined : lastName,
-  thisYear: typeof thisYear === 'undefined' ? undefined : thisYear,
-  birthYear: typeof birthYear === 'undefined' ? undefined : birthYear,
-  greeting: typeof greeting === 'undefined' ? undefined : greeting,
-  fullName: typeof fullName === 'undefined' ? undefined : fullName,
-  age: typeof age === 'undefined' ? undefined : age
-}
+  firstName: typeof firstName === "undefined" ? undefined : firstName,
+  lastName: typeof lastName === "undefined" ? undefined : lastName,
+  thisYear: typeof thisYear === "undefined" ? undefined : thisYear,
+  birthYear: typeof birthYear === "undefined" ? undefined : birthYear,
+  greeting: typeof greeting === "undefined" ? undefined : greeting,
+  fullName: typeof fullName === "undefined" ? undefined : fullName,
+  age: typeof age === "undefined" ? undefined : age,
+};
