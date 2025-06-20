ea66061c50b8104b7bea3b50cfc4868d32894c22
zk18l <zaidalpero63@gmail.com>
2025-06-20 16:16:57 +0300

    exercise 14 done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let newNumber;
+  if (typeof num === "number") {
+    newNumber = num;
+  } else {
+    newNumber = Number(num);
+  }
+  if (newNumber < 0) return "OOPS";
+  let fibonacci1 = 0;
+  let fibonacci2 = 1;
+  let fibonacciDone = 0;
+  for (let index = 1; index < newNumber; index++) {
+    fibonacciDone = fibonacci1 + fibonacci2;
+    fibonacci1 = fibonacci2;
+    fibonacci2 = fibonacciDone;
+  }
+  if (newNumber === 1) fibonacciDone = 1;
+  return fibonacciDone;
 };
 
 // Do not edit below this line
