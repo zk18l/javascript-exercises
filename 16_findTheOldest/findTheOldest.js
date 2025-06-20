4a0950b32a9d9c4e3c86fa95b0a275c409d5b936
zk18l <zaidalpero63@gmail.com>
2025-06-20 17:45:53 +0300

    16 done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (persons) {
+  let oldest = 0;
+  let oldestPerson = "";
+  let temp;
+  for (let key of persons) {
+    if (!("yearOfDeath" in key)) {
+      temp = new Date().getFullYear() - key.yearOfBirth;
+    } else {
+      temp = key.yearOfDeath - key.yearOfBirth;
+    }
+    if (oldest < temp) {
+      oldest = temp;
+      oldestPerson = key;
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
